245a09ba1804d7767710552f5259f99426709db9
xmuyzz <xmuyzz@gmail.com>
2022-05-16 15:04:18 -0400

    curation

---
+++ curation/BCH_curation.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+import numpy as np
+#import zipfile
+import shutil
+
+
+def main(data_dir):
+
+    for file in data_dir:
+        shutil.unpack_archive(filename, extract_dir)
+

0909657cc417709dff3e33d30a812c80632d120a
xmuyzz <xmuyzz@gmail.com>
2022-05-26 18:11:20 -0400

    curation

--- curation/BCH_T2W.py
+++ curation/BCH_T2W.py
@@ -19,10 +19,12 @@ def main(curation_dir, BCH_T2W_dir, pro_data_dir):
     img_dirs = []
     pat_ids = []
     n_slices = []
+    t2w_seqs = []
     for path in l2_dirs:
         pat_id = path.split('/')[-2].split('_')[-1]
         #pat_id = path.split('/')[-2]
         if path.split('.')[-1] == 'gz':
+            t2w_seq = path.split('/')[-1].split('.')[0]
             a = path.split('/')[-1].split('.')[0].split('_')
             #print('a:', a)
             if not list(set(a) & set(['cor', 'sag', 'flair'])):
@@ -30,15 +32,20 @@ def main(curation_dir, BCH_T2W_dir, pro_data_dir):
                     count += 1
                     #fn_dir = BCH_T2W_dir + '/' + pat_id + '.nii.gz'
                     img = nib.load(path)
+                    #meta = str(img.header.extensions)
+                    #print(meta)
                     n_slice = img.shape[2]
                     print(count)
                     print(pat_id)
                     img_dirs.append(path)
                     pat_ids.append(pat_id)
                     n_slices.append(n_slice)
-    df = pd.DataFrame({'pat_id': pat_ids, 'img_dir': img_dirs, 'n_slice': n_slices})
+                    t2w_seqs.append(t2w_seq)
+    df = pd.DataFrame({
+        'pat_id': pat_ids, 'img_dir': img_dirs, 'n_slice': n_slices, 't2w_seq': t2w_seqs})
     pd.set_option('display.max_rows', None)
     #print(df)
+    # get the df with largest slice numbers
     #df = df.sort_values('n_slice', ascending=False).drop_duplicates(['pat_id'])
     df = df.loc[df.groupby(['pat_id'])['n_slice'].idxmax()].reset_index()
     #print(df)
@@ -53,7 +60,7 @@ def main(curation_dir, BCH_T2W_dir, pro_data_dir):
     img_dirs = [i for i in sorted(glob.glob(BCH_T2W_dir + '/*.nii.gz'))]
     #print(img_dirs)
     df['img_dir'] = img_dirs
-    df.columns = ['index', 'bch_mrn', 'img_dir', 'n_slice']
+    df.columns = ['index', 'bch_mrn', 'img_dir', 'n_slice', 't2w_seq']
     meta = pd.read_csv(os.path.join(pro_data_dir, 'BCH_clinical_meta.csv'), on_bad_lines='skip')
     meta['bch_mrn'] = meta['bch_mrn'].astype('float64')
     df['bch_mrn'] = df['bch_mrn'].astype('float64')
--- curation/registration.py
+++ curation/registration.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 
 
+
 def main(pro_data_dir, output_dir, temp_img, save_tfm=False):
 
     """
@@ -30,49 +31,53 @@ def main(pro_data_dir, output_dir, temp_img, save_tfm=False):
     for img_dir, pat_id in zip(df['img_dir'], df['bch_mrn']):
         count += 1
         print(count)
-        moving_image = sitk.ReadImage(img_dir, sitk.sitkFloat32)
-        transform = sitk.CenteredTransformInitializer(
-            fixed_image, 
-            moving_image, 
-            sitk.Euler3DTransform(), 
-            sitk.CenteredTransformInitializerFilter.GEOMETRY)
-        
-        # multi-resolution rigid registration using Mutual Information
-        registration_method = sitk.ImageRegistrationMethod()
-        registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
-        registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
-        registration_method.SetMetricSamplingPercentage(0.01)
-        registration_method.SetInterpolator(sitk.sitkLinear)
-        registration_method.SetOptimizerAsGradientDescent(
-            learningRate=1.0, 
-            numberOfIterations=100, 
-            convergenceMinimumValue=1e-6, 
-            convergenceWindowSize=10)
-        registration_method.SetOptimizerScalesFromPhysicalShift()
-        registration_method.SetShrinkFactorsPerLevel(shrinkFactors = [4, 2, 1])
-        registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
-        registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
-        registration_method.SetInitialTransform(transform)
-        final_transform = registration_method.Execute(
-            fixed_image, 
-            moving_image)                               
-        moving_image_resampled = sitk.Resample(
-            moving_image, 
-            fixed_image, 
-            final_transform, 
-            sitk.sitkLinear, 
-            0.0, 
-            moving_image.GetPixelID())
-        
-        sitk.WriteImage(
-            moving_image_resampled, os.path.join(output_dir, str(int(pat_id)) + '_registered.nii.gz'))
-        if save_tfm:
-            sitk.WriteTransform(final_transform, os.path.join(output_dir, str(int(pat_id)) + '_T2.tfm'))
-    
+        try:
+            moving_image = sitk.ReadImage(img_dir, sitk.sitkFloat32)
+            transform = sitk.CenteredTransformInitializer(
+                fixed_image, 
+                moving_image, 
+                sitk.Euler3DTransform(), 
+                sitk.CenteredTransformInitializerFilter.GEOMETRY)
+            
+            # multi-resolution rigid registration using Mutual Information
+            registration_method = sitk.ImageRegistrationMethod()
+            registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+            registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+            registration_method.SetMetricSamplingPercentage(0.01)
+            registration_method.SetInterpolator(sitk.sitkLinear)
+            registration_method.SetOptimizerAsGradientDescent(
+                learningRate=1.0, 
+                numberOfIterations=100, 
+                convergenceMinimumValue=1e-6, 
+                convergenceWindowSize=10)
+            registration_method.SetOptimizerScalesFromPhysicalShift()
+            registration_method.SetShrinkFactorsPerLevel(shrinkFactors = [4, 2, 1])
+            registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
+            registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+            registration_method.SetInitialTransform(transform)
+            final_transform = registration_method.Execute(
+                fixed_image, 
+                moving_image)                               
+            moving_image_resampled = sitk.Resample(
+                moving_image, 
+                fixed_image, 
+                final_transform, 
+                sitk.sitkLinear, 
+                0.0, 
+                moving_image.GetPixelID())
+            
+            sitk.WriteImage(
+                moving_image_resampled, os.path.join(output_dir, str(int(pat_id)) + '_registered.nii.gz'))
+            if save_tfm:
+                sitk.WriteTransform(final_transform, os.path.join(output_dir, str(int(pat_id)) + '_T2.tfm'))
+        except:
+            print('problematic data:', count, pat_id)
+
+
 
 if __name__ == '__main__':
     
-    temp_img = 't2w_head.nii.gz'
+    temp_img = 't2w_temp.nii.gz'
     proj_dir = '/mnt/aertslab/USERS/Zezhong/pLGG'
     output_dir = os.path.join(proj_dir, 'BCH_T2W_reg')
     pro_data_dir = os.path.join(proj_dir, 'pro_data')
